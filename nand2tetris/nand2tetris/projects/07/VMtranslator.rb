14746415ed021a1bb156e3f01ee3f472c1165ef1
kou-6322 <12151215mei@gmail.com>
2022-02-23 12:45:15 +0900

    second

--- nand2tetris/nand2tetris/projects/07/VMtranslator.rb
+++ nand2tetris/nand2tetris/projects/07/VMtranslator.rb
@@ -1,10 +1,10 @@
-# arg1,2メソッドの場合分けを正確に！
+ #arg1,2メソッドの場合分けを正確に！
 class VMParser
   attr_accessor :current_code, :next_code, :current_code_s, :pc
 
   def initialize(file_name)
     @artithmetic_types = ["add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"]
-    @segment_types = ["argument", "local", "static", "cinstant", "this", "that", "pointer", "temp"]
+    @segment_types = ["argument", "local", "static", "constant", "this", "that", "pointer", "temp"]
     @file = File.open(file_name)
     @pc = 0
   end
@@ -36,27 +36,32 @@ class VMParser
       correct_code?()
       return "C_POP"
     elsif @current_code[0] == "label"
-      @commnand_type = "C_LABEL"
+      @command_type = "C_LABEL"
       correct_code?()
       return "C_LABEL"
     elsif @current_code[0] == "goto"
-      @commnand_type = "C_GOTO"
+      @command_type = "C_GOTO"
       correct_code?()
       return "C_GOTO"
     elsif @current_code[0] == "if-goto"
-      @commnand_type = "C_IF"
+      @command_type = "C_IF"
       correct_code?()
       return "C_IF"
     elsif @current_code[0] == "return"
-      @commnand_type = "C_RETURN"
+      @command_type = "C_RETURN"
       return "C_RETURN"
     elsif @current_code[0] == "call"
       @command_type = "C_CALL"
       correct_code?()
       return "C_CALL"
+    elsif @current_code[0] == "function"
+      @command_type = "C_FUNCTION"
+      correct_code?()
+      return "C_FUNCTION"
     else
       puts "ERROR: '#{@current_code[0]}' in command:#{@pc} is invalid!"
     end
+    puts @command_type
   end
 
   def arg1
@@ -78,6 +83,7 @@ class VMParser
     end
   end
 
+
   private
 
     def correct_code?
@@ -90,41 +96,41 @@ class VMParser
           exit
         end
       when "C_PUSH"
-        if @artithmetic_types.include?(@current_code[1]) && @current_code[2] =~ num && @current_code[3..].nil?
+        if !(@segment_types.include?(@current_code[1]) && @current_code[2] =~ num && @current_code.length == 3)
           puts "ERROR: '#{@current_code_s}' in command:#{@pc} is invalid! expected to be given push code"
           exit
         end
       when "C_POP"
-        if @artithmetic_types.include?(@current_code[1]) && @current_code[2] =~ num && @current_code[3..].nil?
+        if !(@segment_types.include?(@current_code[1]) && @current_code[2] =~ num && @current_code.length == 3)
           puts "ERROR: '#{@current_code_s}' in command:#{@pc} is invalid!  expected to be given pop code"
           exit
         end
       when "C_LABEL"
-        if (@current_code[1] =~ num || @current_code =~ s) && @current_code[2..] == nil
+        if !((@current_code[1] =~ num || @current_code[1] =~ s) && @current_code.length == 2)
           puts "ERROR: '#{@current_code_s}' in command:#{@pc} is invalid! expected to be given label code"
         end
       when "C_GOTO"
-        if (@current_code[1] =~ num || @current_code =~ s) && @current_code[2..] == nil
+        if !((@current_code[1] =~ num || @current_code[1] =~ s) && @current_code.length == 2)
           puts "ERROR: '#{@current_code_s}' in command:#{@pc} is invalid! expected to be given goto code"
           exit
         end
       when "C_IF"
-        if (@current_code[1] =~ num || @current_code =~ s) && @current_code[2..] == nil
+        if !((@current_code[1] =~ num || @current_code[1] =~ s) && @current_code.length == 2)
           puts "ERROR: '#{@current_code_s}' in command:#{@pc} is invalid! expected to be given if code"
           exit
         end
       when "C_FUNCTION"
-        if @current_code[1] =~ s && @current_code[2] =~ num && @current_code.length == 3
+        if !(@current_code[1] =~ s && @current_code[2] =~ num && @current_code.length == 3)
           puts "ERROR: '#{@current_code_s}' in command:#{@pc} is invalid! expected to be given function code"
           exit
         end
       when "C_CALL"
-        if @current_code[1] =~ s && @current_code[2] =~ num && @current_code.length == 3
+        if !(@current_code[1] =~ s && @current_code[2] =~ num && @current_code.length == 3)
           puts "ERROR: '#{@current_code_s}' in command:#{@pc} is invalid! expected to be given call code"
           exit
         end
+      end
     end
-
     def read_next
       @pc += 1
       i_code =  @file.gets
@@ -136,27 +142,32 @@ class VMParser
           i_code = read_next()
         end
         return i_code
-      else
       end
     end
 end
 
 class CodeWriter
 
-  def initialize(file_name)
-    @wfile = File.open("#{file_name}.asm","w")
+  def initialize()
     @pc=0
+    @function_counter = 0
+  end
+
+  def setup
     s = ""
     s += "@256\n"
     s += "D=A\n"
     s += "@0\n"
     s += "M=D\n"
     s += "\n"
-    @wfile.puts s
+    s += "\n"
+    s += self.writeCall("Sys.init",0)
     puts s
+    return s
   end
 
-  def setFileName
+  def setFileName(f_name)
+    @current_file = f_name
   end
 
   def writeArithmetic(command)
@@ -279,11 +290,12 @@ class CodeWriter
     s+="A=A\n"
     s+="A=A\n"
     s+="A=A\n"
-    s+="//#{@pc}\n"
+    #s+="//#{@pc}\n"
     @pc+=1
     puts s
-    @wfile.puts s
+    return s
   end
+
 
   def writePushPop(command, segment, index)
     @segments = {"local" => "LCL", "argument" => "ARG", "this" => "THIS", "that" => "THAT", "temp" => "5", "pointer" => "3", "static" => "16"}
@@ -297,8 +309,16 @@ class CodeWriter
         s += "M=D\n"
         s += "@SP\n"
         s += "M=M+1\n"
+      elsif segment == "static"
+        s += "@#{@current_file}.#{index}\n"
+        s += "D=M\n"
+        s += "@SP\n"
+        s += "A=M\n"
+        s += "M=D\n"
+        s += "@SP\n"
+        s += "M=M+1\n"
       else
-        if segment == "temp" || segment == "pointer" || segment == "static"
+        if segment == "temp" || segment == "pointer"
           s += "@#{@segments[segment]}\n"
           s += "D=A\n"
         else
@@ -321,12 +341,17 @@ class CodeWriter
       s += "D=M\n"
       s += "@13\n"
       s += "M=D\n"
-      s += "@#{index}\n"
-      s += "D=A\n"
-      if segment == "temp" || segment == "pointer" || segment == "static"
+      if segment == "temp" || segment == "pointer"
+        s += "@#{index}\n"
+        s += "D=A\n"
         s +="@#{@segments[segment]}\n"
         s += "D=A+D\n"
+      elsif segment == "static"
+        s += "@#{@current_file}.#{index}\n"
+        s += "D=A\n"
       else
+        s += "@#{index}\n"
+        s += "D=A\n"
         s +="@#{@segments[segment]}\n"
         s += "D=M+D\n"
       end
@@ -341,10 +366,10 @@ class CodeWriter
     s+="A=A\n"
     s+="A=A\n"
     s+="A=A\n"
-    s+="//#{@pc}\n"
+    #s+="//#{@pc}\n"
     @pc+=1
     puts s
-    @wfile.puts s
+    return s
   end
 
   def writeLabel(label)
@@ -352,10 +377,10 @@ class CodeWriter
     s+="A=A\n"
     s+="A=A\n"
     s+="A=A\n"
-    s+="//#{@pc}\n"
+    #s+="//#{@pc}\n"
     @pc+=1
     puts s
-    @wfile.puts s
+    return s
   end
 
   def writeGoto(label)
@@ -365,10 +390,10 @@ class CodeWriter
     s+="A=A\n"
     s+="A=A\n"
     s+="A=A\n"
-    s+="//#{@pc}\n"
+    #s+="//#{@pc}\n"
     @pc+=1
     puts s
-    @wfile.puts s
+    return s
   end
 
   def writeIf(label)
@@ -382,43 +407,178 @@ class CodeWriter
     s+="A=A\n"
     s+="A=A\n"
     s+="A=A\n"
-    s+="//#{@pc}\n"
+    #s+="//#{@pc}\n"
     @pc+=1
     puts s
-    @wfile.puts s
+    return s
   end
 
-  def writeCall
+  def writeCall(functionName, numArgs)
+    @function_counter += 1
     s = ""
-    s += "@\n"
-    s+="A=A\n"
-    s+="A=A\n"
-    s+="A=A\n"
-    s+="//#{@pc}\n"
+    s += "@#{functionName}$#{@function_counter}\n"
+    s += "D=A\n"
+    s += "@0\n"
+    s += "M=M+1\n"
+    s += "A=M-1\n"
+    s += "M=D\n"
+    s += "@1\n"
+    s += "D=M\n"
+    s += "@SP\n"
+    s += "M=M+1\n"
+    s += "A=M-1\n"
+    s += "M=D\n"
+    s += "@2\n"
+    s += "D=M\n"
+    s += "@SP\n"
+    s += "M=M+1\n"
+    s += "A=M-1\n"
+    s += "M=D\n"
+    s += self.writePushPop("C_PUSH", "pointer", "0")
+    s += self.writePushPop("C_PUSH", "pointer", "1")
+    s += "@5\n"
+    s += "D=A\n"
+    s += "@#{numArgs}\n"
+    s += "D=D+A\n"
+    s += "@0\n"
+    s += "D=M-D\n"
+    s += "@2\n"
+    s += "M=D\n"
+    s += "@0\n"
+    s += "D=M\n"
+    s += "@1\n"
+    s += "M=D\n"
+    s += "@#{functionName}\n"
+    s += "0;JMP\n"
+    s += "(#{functionName}$#{@function_counter})\n"
+    s +="A=A\n"
+    s +="A=A\n"
+    s +="A=A\n"
+    #s+="//#{@pc}\n"
     @pc+=1
     puts s
-    @wfile.puts s
+    return s
+  end
 
+  def writeFunction(functionName, numLocals)
+    s = ""
+    s += "(#{functionName})\n"
+    numLocals.to_i.times do
+      s += self.writePushPop("C_PUSH", "constant", "0")
+    end
+    s+="A=A\n"
+    s+="A=A\n"
+    s+="A=A\n"
+    #s+="//#{@pc}\n"
+    @pc+=1
+    puts s
+    return s
+  end
+
+  def writeReturn
+    s = ""
+    s += "@1\n"
+    s += "D=M\n"
+    s += "@13\n"
+    s += "M=D\n"
+    s += "@5\n"
+    s += "D=D-A\n"
+    s += "@14\n"
+    s += "M=D\n"
+    s += "@0\n"
+    s += "M=M-1\n"
+    s += "A=M\n"
+    s += "D=M\n"
+    s += "@2\n"
+    s += "A=M\n"
+    s += "M=D\n"
+    s += "@2\n"
+    s += "D=M\n"
+    s += "@0\n"
+    s += "M=D+1\n"
+    s += "@13\n"
+    s += "D=M\n"
+    s += "D=D-1\n"
+    s += "A=D\n"
+    s += "D=M\n"
+    s += "@4\n"
+    s += "M=D\n"
+    s += "@2\n"
+    s += "D=A\n"
+    s += "@13\n"
+    s += "D=M-D\n"
+    s += "A=D\n"
+    s += "D=M\n"
+    s += "@3\n"
+    s += "M=D\n"
+    s += "D=A\n"
+    s += "@13\n"
+    s += "D=M-D\n"
+    s += "A=D\n"
+    s += "D=M\n"
+    s += "@2\n"
+    s += "M=D\n"
+    s += "@4\n"
+    s += "D=A\n"
+    s += "@13\n"
+    s += "D=M-D\n"
+    s += "A=D\n"
+    s += "D=M\n"
+    s += "@1\n"
+    s += "M=D\n"
+    s += "@14\n"
+    s += "A=M\n"
+    s += "A=M\n"
+    s += "0;JMP\n"
+    s+="A=A\n"
+    s+="A=A\n"
+    s+="A=A\n"
+    #s+="//#{@pc}\n"
+    @pc+=1
+    puts s
+    return s
+  end
 
 end
-
 /main/
-rfile = VMParser.new(ARGV[0])
-wfile = CodeWriter.new(ARGV[0].split("")[0..-4].join)
+require 'find'
 current_function = ""
-while rfile.hasMoreCommands
-  rfile.advance
-  type = rfile.commandType
-  if type == "C_ARTITHMETIC"
-    wfile.writeArithmetic(rfile.current_code_s)
-  elsif type == "C_POP" || type == "C_PUSH"
-    wfile.writePushPop(type, rfile.current_code[1], rfile.current_code[2])
-  elsif type == "C_LABEL"
-    wfile.writeLabel("#{current_function}$#{rfile.current_code[1]}")
-  elsif type == "C_GOTO"
-    wfile.writeGoto("#{current_function}$#{rfile.current_code[1]}")
-  elsif type == "C_IF"
-    wfile.writeIf("#{current_function}$#{rfile.current_code[1]}")
+path = ARGV[0] ? ARGV[0] : '.'
+wfile = File.open("#{ARGV[0]}.asm","w")
+code_writer = CodeWriter.new
+wfile.puts (code_writer.setup)
+Find.find(path) do |item|
+  base = File.basename(item)
+  if base.split("")[-3..-1].join == ".vm"
+    code_writer.setFileName(base)
+    rfile = VMParser.new(item)
+    while rfile.hasMoreCommands
+      rfile.advance
+      type = rfile.commandType
+      case type
+      when "C_ARTITHMETIC"
+        wfile.puts code_writer.writeArithmetic(rfile.current_code[0])
+      when "C_POP"
+        wfile.puts code_writer.writePushPop("C_POP", rfile.current_code[1], rfile.current_code[2])
+      when "C_PUSH"
+        wfile.puts code_writer.writePushPop("C_PUSH", rfile.current_code[1], rfile.current_code[2])
+      when "C_LABEL"
+        wfile.puts code_writer.writeLabel("#{current_function}$#{rfile.current_code[1]}")
+      when "C_GOTO"
+        wfile.puts code_writer.writeGoto("#{current_function}$#{rfile.current_code[1]}")
+      when "C_IF"
+        wfile.puts code_writer.writeIf("#{current_function}$#{rfile.current_code[1]}")
+      when "C_CALL"
+        wfile.puts code_writer.writeCall(rfile.current_code[1], rfile.current_code[2])
+      when "C_FUNCTION"
+        wfile.puts code_writer.writeFunction(rfile.current_code[1], rfile.current_code[2])
+        current_function = rfile.current_code[1]
+      when "C_RETURN"
+        wfile.puts code_writer.writeReturn
+      end
+    end
   end
 end
+
+
 puts "Translating is Done! We made '.asm' File on same Directory."
